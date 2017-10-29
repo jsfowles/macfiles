46da15651a77be027851a21e96046678ec65c91b
Jacob Fowles <jacob@underbelly.is>
2017-10-29 17:41:12 -0600

    Update caskfiles.sh

--- caskfiles.sh
+++ caskfiles.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
+
+# Brew itself
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
 # homebrew taps
 brew tap caskroom/cask
 brew tap caskroom/versions
